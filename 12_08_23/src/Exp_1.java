25b0385147ffb3621c071288df726f08c1bdbe97
Danilo Pereira <d4n_kali@protonmail.com>
2024-08-13 15:07:12 -0300

    Create Exp_1.java

---
+++ 12_08_23/src/Exp_1.java
@@ -0,0 +1,15 @@
+// Exemplo 1
+
+public class Exp_1 {
+
+    public static void main(String[] args) throws Exception {
+    
+        double num1 = 1;
+        double num2 = 3;
+        double soma = num1 + num2;
+
+        System.out.println("A soma do numero 1: "+num1+" mais do numero 2: "+num2+" é: "+soma);
+
+    }
+
+}
