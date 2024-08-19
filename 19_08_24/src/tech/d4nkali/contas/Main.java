2f19b9821e99567db45c7d9e50419c8737ea7df3
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-08-19 20:53:55 -0300

    Adicionando objeto conta 1

--- 19_08_24/src/tech/d4nkali/contas/Main.java
+++ 19_08_24/src/tech/d4nkali/contas/Main.java
@@ -3,7 +3,17 @@ package tech.d4nkali.contas;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Hello, World!");
+		Conta conta1 = new Conta(); //* Criando objeto
+
+		// Utilização dos estados
+
+			conta1.limite = 100000;
+			conta1.saldo = 50000;
+			conta1.nome = "Junin do Carro";
+			conta1.numero = 01;
+
+		conta1.sacar(650); //* Utilização do Comportamento
 
     }
+
 }
