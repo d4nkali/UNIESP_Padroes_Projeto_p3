66d80addbee3d4141afea5bbf21ae5eb5de55bd0
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-09-09 20:53:03 -0300

    Adicionando deposito e saque

--- 09_09_24/src/tech/d4nkali/contas/ContaInvestimento.java
+++ 09_09_24/src/tech/d4nkali/contas/ContaInvestimento.java
@@ -2,6 +2,38 @@ package tech.d4nkali.contas;
 
 public class ContaInvestimento extends Conta{
 
-	
+	double taxa;
+	int prazo;
+
+	boolean sacar (double quantidade) {
+
+		if (this.saldo < quantidade) {
+			return false;
+		}
+
+		else {
+
+			double novoSaldo = this.saldo - quantidade * 1.01;
+			this.saldo = novoSaldo;
+			return true;
+
+		}
+
+	}
+
+	boolean depositar (double quantidade) {
+
+		if (quantidade == 0 || quantidade < 0) {
+			return false;
+		}
+
+		else {
+
+			this.saldo = this.saldo + quantidade * 0.01;
+			return true;
+
+		}
+
+	}
 
 }
