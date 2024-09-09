a492e642ae27c2025bc1bad3e355f1a669f5b509
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-09-09 20:35:27 -0300

    Adicionando get set

--- 09_09_24/src/tech/d4nkali/contas/ContaEspecial.java
+++ 09_09_24/src/tech/d4nkali/contas/ContaEspecial.java
@@ -4,4 +4,12 @@ public class ContaEspecial extends Conta {
 
 	double limite;
 
+	public double getLimite() {
+		return limite;
+	}
+
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+
 }
