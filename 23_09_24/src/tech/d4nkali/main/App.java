26ba07389953cc3073b84f302f2444b2f9ddf782
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-09-23 19:47:44 -0300

    Adicionando App

---
+++ 23_09_24/src/tech/d4nkali/main/App.java
@@ -0,0 +1,13 @@
+package tech.d4nkali.main;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+		// Cadastrar Clientes
+
+
+		// Cadastrar Funcionários
+
+    }
+}
